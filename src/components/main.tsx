06970c9608a01d2e16bdeb12499695a00725a510
isaneyde <issitoe941@gmail.com>
2025-05-26 00:23:42 +0200

    update:update the add task function

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -2,20 +2,25 @@ import { CheckCircleIcon, PlusCircleIcon } from "@phosphor-icons/react"
 import { TrashIcon } from "@phosphor-icons/react/dist/ssr";
 import { useState } from "react"
 export const Main = () => {
-    const [task, setTask] = useState(" ");
-    const [isHidden, setIsHidden] = useState(false);
-    const [list,setList]=useState(0);
-    const add=()=>{
-        setTask(addList=>addList+1.)
+    const [task, setTask] = useState("");
+    const [isHidden, setIsHidden] = useState(true);
+    const [list,setList]=useState([]);
+  
+    const addTask=()=>{
+        setIsHidden(!isHidden);
+         setList([...list,]); 
+            {list.map((list)=>setList.length+1)}
+            
+        
     }
     return (
+    
         <main className="bg-white h-screen justify-items-center">
             <fieldset className="fieldset">
                 <input type="text" className="input text-center bg-emerald-200 text-black  mt-5"
-                    placeholder="What is the task?" value={task} onChange={(event) => setTask(event.target.value)} />
-                <button onClick={()=>setIsHidden(!isHidden)}><PlusCircleIcon size={32} color="green"
+                    placeholder="What is the task?" value={task} onChange={(event) => setTask(event.target.value)}/>
+                <button onClick={addTask}><PlusCircleIcon size={32} color="green"
                     className="ml-85 -mt-10" /></button>
-
             </fieldset>
             <div style={{ display: isHidden ? 'none' : 'block' }} >
                 <div className="w-80 h-10 rounded 2xl 
@@ -24,10 +29,12 @@ export const Main = () => {
                     <button> <CheckCircleIcon size={32} color="green" className="" /></button>
                     <button onClick={() => setIsHidden(!isHidden)}> <TrashIcon size={32} color="red" className="" /></button>
                 </div>
+
             </div>
-
+            
+    
         </main>
-
+    
 
     )
 
